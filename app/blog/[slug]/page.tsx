7121f5a06564a059da1a5c9efa1b35d314c7f0e1
ndbaolam <baolam782004@gmail.com>
2024-09-04 09:05:23 +0700

    new update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -12,7 +12,7 @@ export default function Page({ params }: Params) {
   return (
     <>  
       <h1>Slug: {params.slug}</h1>
-      <p>HoleTex</p>
+      <p>Hole Tex</p>
       <p>Welcome to CICD</p>
     </>    
   );
